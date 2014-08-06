2edd21da068f7847c41b2fe05a6a1be09f0e91c6
Yorji Perez <havsoftox@gmail.com>
2014-08-05 21:06:52 -0400

    delete tabs graph update config

--- config/config.js
+++ config/config.js
@@ -1,8 +1,7 @@
 apiConfig = {
-    'gr_username': 'alain@hlasolutionsgroup.com',
-    'gr_auth_token': "8450103c06dbd58add9d047d761684096ac560ca",
-    'gr_rfp_account': 'genius-referrals',
-    'gr_rfp_campaign': 'get-15-for-90-days-1',
-    'gr_rfp_widgets_package': 'genius-referrals-default-2'
+    'gr_username': 'YOUR_USERNAME',
+    'gr_auth_token': "YOUR_API_TOKEN",
+    'gr_rfp_account': 'YOUR_ACCOUNT_SLUG',
+    'gr_rfp_campaign': 'YOUR_CAMPAIGN_SLUG',
+    'gr_rfp_widgets_package': 'YOUR_WIDGETS_PACKAGE_SLUG'
 };
-
